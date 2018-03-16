5e29a602e053e2c66212e78481322b5dec1a2e41
zhoulong <long.zhou@xfxb.net>
2018-03-16 14:42:30 +0800

    基础项目搭建

--- excel-processing/src/main/java/writer/XSSFCellStyleUtil.java
+++ excel-processing/src/main/java/writer/XSSFCellStyleUtil.java
@@ -131,41 +131,5 @@ public class XSSFCellStyleUtil {
         cellStyle.setBorderTop(BorderStyle.THIN);
         return cellStyle;
     }
-
-    /**
-     * 大表头样式定义
-     *
-     * @return
-     */
-    public static XSSFCellStyle getOrderDataBigHeadStyle(SXSSFWorkbook wb) {
-        // 创建单元格样式
-        XSSFCellStyle cellStyle = wb.getXSSFWorkbook().createCellStyle();
-        // 设置单元格的背景颜色为淡蓝色
-        // 设置单元格的背景颜色为淡蓝色
-        cellStyle.setFillForegroundColor(IndexedColors.LIGHT_GREEN.index);
-        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        // 设置单元格居中对齐
-        cellStyle.setAlignment(HorizontalAlignment.CENTER);
-        // 设置单元格垂直居中对齐
-        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-        // 创建单元格内容显示不下时自动换行
-        cellStyle.setWrapText(true);
-        // 设置单元格字体样式
-        XSSFFont font = wb.getXSSFWorkbook().createFont();
-        // 设置字体加粗
-        font.setBold(true);
-        //font.setFontName("宋体");
-        font.setFontName("微软雅黑");
-        font.setFontHeightInPoints((short) 20);
-
-        cellStyle.setFont(font);
-        // 设置单元格边框为细线条
-       /* cellStyle.setBorderLeft(BorderStyle.THIN);
-        cellStyle.setBorderBottom(BorderStyle.THIN);
-        cellStyle.setBorderRight(BorderStyle.THIN);
-        cellStyle.setBorderTop(BorderStyle.THIN);*/
-        return cellStyle;
-    }
-
-
+    
 }
